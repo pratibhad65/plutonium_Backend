614943f9b9942278bcf8ef67f30890d6541811b5
Pratibha <pratibhad65@gmail.com>
2022-08-09 13:07:27 +0530

    updated

--- src/routes/route.js
+++ src/routes/route.js
@@ -15,24 +15,50 @@ router.get('/students', function (req, res){
     res.send(students)
 })
 
-router.get('/student-details/:name', function(req, res){
-    /*
-    params is an attribute inside request that contains 
-    dynamic values.
-    This value comes from the request url in the form of an 
-    object where key is the variable defined in code 
-    and value is what is sent in the request
-    */
+router.get("/movies/:indexNumber", function(req, res){
+    const movies = ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
+    console.log(req.params.indexNumber)
+    let movieIndex = req.params.indexNumber
+    //check index value. less than 0 or greater than array (length - 1) are not valid
+    if(movieIndex<0 || movieIndex>=movies.length) {
+        //if the index is invalid send an error message
+        return res.send('The index value is not correct, Please check the it')
+    }
 
-    let requestParams = req.params
-
-    // JSON strigify function helps to print an entire object
-    // We can use any ways to print an object in Javascript, JSON stringify is one of them
-    console.log("This is the request "+ JSON.stringify(requestParams))
-    let studentName = requestParams.name
-    console.log('Name of the student is ', studentName)
-    
-    res.send('Dummy response')
+    //if the index was valid send the movie at that index in response
+    let requiredMovie = movies[movieIndex]
+    res.send(requiredMovie)
 })
+
+router.get("/films", function(req, res){
+    const films = [ {
+        "id": 1,
+        "name": "The Shining"
+       }, {
+        "id": 2,
+        "name": "Incendies"
+       }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+       }, {
+        "id": 4,
+        "name": "Finding Nemo"
+       }]
+       //send all the films
+      res.send(films) 
+})
+//iterate all the films
+       //search for a film whose id matches with the id recevied in request
+       for(let i = 0; i < films.length; i++){
+        let film = films[i]
+        if(film.id == filmId) {
+            //if there is a match return the response from here
+            return res.send(film)
+        }
+    }
+
+    //if there is no match give an error response
+    res.send("The film id doesn't match any movie")
+
 
 module.exports = router;
