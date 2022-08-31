bf90cecba688948f182624f5e3c32f5174d8f9f3
Pratibha <pratibhad65@gmail.com>
2022-08-31 21:56:25 +0530

    auth-validation

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://FunctionUp-Cohort_Pratibha:XTiZIZDxatT9x2cN@cluster0.74qavwu.mongodb.net/New-Authentication?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -14,8 +14,44 @@ const userSchema = new mongoose.Schema( {
         type: String,
         enum: ["male", "female", "other"]
     },
+    isDeleted: {
+        deafult:false
+    },
     age: Number,
     posts: {type: [], deafult: []}
 }, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model('UserModel', userSchema)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const createUser = async function (req, res) {
+//     try{
+//     let data = req.body;
+//     console.log(data)
+//     if(Object.keys(data).length !=0){
+//     let savedData = await userModel.create(data);
+//     res.status(201).send({msg: savedData})
+//     } 
+//     else res.status(400).send({msg:"BAD REQUEST"})
+//   }
+//   catch(err){
+//     console.log("This is the error : ",err.message);
+//     res.status(500).send({ msg: "Error", error: err.message });
+//   };
