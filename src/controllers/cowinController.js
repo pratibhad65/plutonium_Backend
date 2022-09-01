7be7174ee77064db3745b9640b4d3d814afe6867
Pratibha <pratibhad65@gmail.com>
2022-09-01 23:46:00 +0530

    covin sucessful

--- src/controllers/cowinController.js
+++ src/controllers/cowinController.js
@@ -79,7 +79,36 @@ let getOtp = async function (req, res) {
 }
 
 
+//-------------------------------------------------------------------------------------------=====
+
+//WRITE A GET API TO GET THE LIST OF ALL THE "vaccination sessions by district id" for any
+// given district id and for any given date
+
+  let getAllDistricts = async function (req, res) {
+    try {
+        let district = req.query.districtId
+        let date = req.query.date
+        let options = {
+            method: "get",
+            url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${district}&date=${date}`
+        }
+        let result = await axios(options);
+        console.log(result)
+        let data = result.data
+        res.status(200).send({ msg: data, status: true })
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
+
+//--------------------------------------------------------------------------------------------------------------
+
+
+
 module.exports.getStates = getStates
 module.exports.getDistricts = getDistricts
 module.exports.getByPin = getByPin
 module.exports.getOtp = getOtp
+module.exports.getAllDistricts = getAllDistricts
